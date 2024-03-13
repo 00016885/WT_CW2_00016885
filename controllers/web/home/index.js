566e1347e58b90063275a1517ccca95f645f3294
00016885 <wiuts11@gmail.com>
2024-03-13 17:53:46 +0500

    added add and update

--- controllers/web/home/index.js
+++ controllers/web/home/index.js
@@ -1,7 +1,14 @@
 const home_controller = {
     index: async (req, res) =>{
         res.render('home');
-    }  
+    },
+    add: async (req, res) =>{
+        res.render('home/add');
+    },
+    update: async (req, res) =>{
+        res.render('home/add');
+    }
+  
 };
   
 module.exports = home_controller;
